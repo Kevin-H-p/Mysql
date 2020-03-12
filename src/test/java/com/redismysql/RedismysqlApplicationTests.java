9f27751d2793bd30038b614d788e056197b9bb09
kevin <861835827@qq.com>
2020-03-12 15:03:52 +0800

    test

---
+++ src/test/java/com/redismysql/RedismysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redismysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedismysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
